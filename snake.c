d73e53b050f80611935d9fd2f19f974a3f6cdab6
Talris <b_vitall@mail.ru>
2022-01-20 18:13:48 +0300

    fixed functions to increase the snake`s length and snake`s move

--- snake.c
+++ snake.c
@@ -5,21 +5,42 @@ enum { key_escape = 27, key_pause = 32 };
 
 typedef struct snake {
 	int cur_x, cur_y, dx, dy;
-	struct snake *next;
+	struct snake *next, *prev;
 } snake;
 
-snake *check(snake *s, snake *n);
+void check(snake **s, snake **n);
+void random_star(snake **n);
 
 
+void init(snake **head, snake **n, int *row, int *col) {
+		snake *tmp = malloc(sizeof(snake));
+		tmp->cur_x = *col / 2;
+		tmp->cur_y = *row / 2;
+		tmp->next = NULL;
+		tmp->prev = NULL;
+		*head = tmp;
+		
+		snake *new_el = malloc(sizeof(snake));
+		*n = new_el;
+		random_star(n);
+}
 
-
-
-snake *increase_snake(snake *head, snake *new_el) {	// stack // snake **tail
-	// snake *tmp = malloc(sizeof(snake));
-	new_el->next = head->next;
-	head->next = new_el;
+void increase_snake(snake *head, snake *new_el) {	// snake **tail
+	snake *tmp = malloc(sizeof(snake));
+	tmp->cur_x = new_el->cur_x;
+	tmp->cur_y = new_el->cur_y;
 	
-	return head;
+	tmp->next = head->next;
+	tmp->prev = head;
+	if(tmp->next)
+		tmp->next->prev = tmp;
+	
+	head->next = tmp;
+	static int n = 1;
+	move(2, 0);
+	printw("added:%d", n);
+	n++;
+	// return head;
 }
 
 void hide_star(snake *s) {
@@ -34,68 +55,79 @@ void hide_star(snake *s) {
 
 void show_star(snake *s) {
 	//snake *tmp = s;
-	int n = 0;
+	int n = 3;
 	while(s != NULL) {
 	
 		move(s->cur_y, s->cur_x);
 		addch('*');
+		
+		move(n, 0);
+		printw("move(%d, %d)", s->cur_y, s->cur_x);
+		
 		s = s->next;
 		n++;
 	}
-	move(1, 0);
-	printw("%d", n);
+	//move(1, 0);
+	//printw("%d", n);
 	refresh();
 	
 }
 
-snake *one_step(snake *s) {
+void step(snake *s) {
 	// int x, y;
-	snake *tmp = s;
-	while(tmp->next != NULL) {
-		tmp->next->cur_x = tmp->cur_x;
-		tmp->next->cur_y = tmp->cur_y;
-		tmp = tmp->next;
+	// snake **tmp = s;
+	if (s != NULL) {
+		//return NULL;
+	
+		step(s->next);
+		if(s->prev != NULL) {
+		//return s;
+		
+			s->cur_x = s->prev->cur_x;
+			s->cur_y = s->prev->cur_y;
+		}
+	
 	}
+	//return s;
 	
-	return s;
 }
 
 
-void move_snake(snake *s, snake *n) {
-	hide_star(s);
-	s = one_step(s);
-	s->cur_x += s-> dx;		// new coord of head
-	s->cur_y += s-> dy;
+void move_snake(snake **s, snake **n) {
+	hide_star(*s);
+	
+	if((*s)->next && ((*s)->cur_x != (*s)->next->cur_x ||
+			(*s)->cur_y != (*s)->next->cur_y)) 
+	{
+		step(*s);
+
+	}
+	(*s)->cur_x += (*s)-> dx;		// new coord of head
+	(*s)->cur_y += (*s)-> dy;
+	
+	check(s, n);
 	
 	
-	s = check(s, n);
 	
-	
-	
-	show_star(s);
+	show_star(*s);
 }
 
-void set_direction(snake *s, int dx, int dy) {
-	s->dx = dx;
-	s->dy = dy;
+void set_direction(snake **s, int dx, int dy) {
+	(*s)->dx = dx;
+	(*s)->dy = dy;
 }
 
-snake *check(snake *s, snake *n) {
-	if(s->cur_x == n->cur_x && s->cur_y == n->cur_y) {
-		snake *tmp = malloc(sizeof(snake));
-		// tmp = n;
-		
-		// tmp->cur_x += s-> dx;
-		// tmp->cur_y += s-> dy;
-		s = increase_snake(s, n);
-		//hide_star(n);
-		//free(n);
-		
+void check(snake **s, snake **n) {
+	if((*s)->cur_x == (*n)->cur_x && (*s)->cur_y == (*n)->cur_y) {
+
+		increase_snake(*s, *n);
+
+		random_star(n);
 		move(0, 0);
 		printw("connected");
 	}
 	
-	return s;
+	// return s;
 }
 
 void game_board(int row, int col) {
@@ -115,26 +147,32 @@ void pause(int is_paused) {
 	//long time = 31536000000; // year
 	
 }
-snake *random_star() {
-	snake *tmp = malloc(sizeof(snake));
-	tmp->cur_x = 45;
-	tmp->cur_y = 12;
-	tmp->next = NULL;
+void random_star(snake **n) {
+	int x, y;
+	y = rand() % 24;
+	x = rand() % 80;
+	// snake *tmp = malloc(sizeof(snake));
 	
-	move(tmp->cur_y, tmp->cur_x);
+	(*n)->cur_x = x;
+	(*n)->cur_y = y;
+	(*n)->next = NULL;
+	
+	move((*n)->cur_y, (*n)->cur_x);
 	addch('*');
+
 	refresh();
 	
-	return tmp;
+	// return tmp;
 }
 int main() {
 	int key, row, col;// is_paused;
 	
 	initscr();
 	cbreak();
-	noecho();
 	keypad(stdscr, 1);
-
+	noecho();
+	curs_set(0);
+	
 	getmaxyx(stdscr, row, col);
 	
 	//game_board(row, col);
@@ -143,36 +181,31 @@ int main() {
 	snake *head = NULL;
 	snake *n = NULL;
 	//snake *tail = NULL;
-	head = malloc(sizeof(snake));
-	head->cur_x = col / 2;
-	head->cur_y = row / 2;
-	head->next = NULL;
-
+	
+	init(&head, &n, &row, &col);
 	// is_paused = 0;
 	
-	//head = increase_snake(head, &s); 
 	show_star(head);
 
-	n = random_star();
 	while((key = getch()) != key_escape) {
 		switch(key) {
 			// case key_pause:
 			
 			case KEY_UP:
-				set_direction(head, 0, -1);
-				move_snake(head, n);
+				set_direction(&head, 0, -1);
+				move_snake(&head, &n);
 				break;
 			case KEY_RIGHT:
-				set_direction(head, 1, 0);
-				move_snake(head, n);
+				set_direction(&head, 1, 0);
+				move_snake(&head, &n);
 				break;
 			case KEY_DOWN:
-				set_direction(head, 0, 1);
-				move_snake(head, n);
+				set_direction(&head, 0, 1);
+				move_snake(&head, &n);
 				break;
 			case KEY_LEFT:
-				set_direction(head, -1, 0);
-				move_snake(head, n);
+				set_direction(&head, -1, 0);
+				move_snake(&head, &n);
 				break;
 		}
 		
